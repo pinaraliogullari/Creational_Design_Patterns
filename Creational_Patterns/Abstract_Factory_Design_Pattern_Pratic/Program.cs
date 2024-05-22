fd7032acb2b3b7f0982e0bbaa1654bc594173980
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-05-22 14:26:05 +0300

    Abstract Factory Design Pattern example completed.

---
+++ Creational_Patterns/Abstract_Factory_Design_Pattern_Pratic/Program.cs
@@ -0,0 +1,131 @@
+﻿//SADECE ABSTRACT FACTORY DESIGN PATTERN KULLANILDI
+
+
+//üreteceğimiz nesnenin alt parçacıklarına da ihitacımız varsa yani onlardan da nesne üretmemiz söz konusu ise Abstract Factory Design Pattern kullanmalıyız. Herhangi bir sınıf başka herhangi bir sınıftan constructor/ property vs vasıtasıyla nesne alabilir ancak burda kontrol etmemiz gereken bu nesneler ilişki nesneler midir, bir ürün ailesine bağlı mıdır? eğer değilse sadece işlevsel amaçla kullanılan dış kaynak bir nesne ise  abstract factory söz konusu değildir.
+
+//Computer computer1 = new();
+
+//CPU cpu = new();
+//computer1.CPU = cpu;
+
+//RAM ram = new();
+//computer1.RAM = ram;
+
+//VideoCard videoCard = new();
+//computer1.VideoCard = videoCard;
+
+//Computer computer2 = new(new(), new(), new());
+
+//CPU cpu2 = ;
+//computer2.CPU = cpu2;
+
+//RAM ram2 = new();
+//computer2.RAM = ram2;
+
+//VideoCard videoCard2 = new();
+//computer2.VideoCard = videoCard2;
+
+ComputerCreator creator = new();
+Computer asus=creator.CreateComputer(new AsusFactory());
+Computer toshiba=creator.CreateComputer(new ToshibaFactory());
+class Computer
+{
+    public Computer(CPU cpu,RAM ram,VideoCard videoCard)
+    {
+        CPU = cpu;
+		RAM = ram;
+		VideoCard= videoCard;
+    }
+    public Computer()
+    {
+        
+    }
+    public CPU CPU { get; set; }
+    public RAM RAM { get; set; }
+    public VideoCard VideoCard { get; set; }
+}
+
+#region Abstract Products
+interface ICPU { }
+interface IRAM { }
+interface IVideoCard { }
+#endregion
+
+#region Concrete Products
+class CPU:ICPU
+{
+	public CPU(string text) => Console.WriteLine(text);
+}
+
+class RAM:IRAM
+{
+	public RAM(string text) => Console.WriteLine(text);
+}
+
+class VideoCard:IVideoCard
+{
+	public VideoCard(string text) => Console.WriteLine(text);
+}
+#endregion
+
+#region Abstract Factory
+interface IComputerFactory
+{
+    ICPU CreateCPU();
+    IRAM CreateRAM();
+    IVideoCard CreateVideoCard();
+}
+#endregion
+
+#region Concrete Factories
+class AsusFactory : IComputerFactory
+{
+	public ICPU CreateCPU()
+		=> new CPU("Asus CPU üretildi");
+
+	public IRAM CreateRAM() 
+		=> new RAM("Asus RAM üretildi");
+
+	public IVideoCard CreateVideoCard()
+		=> new VideoCard("Asus VideoCard üretildi");
+}
+class ToshibaFactory : IComputerFactory
+{
+	public ICPU CreateCPU()
+		=> new CPU("Toshiba CPU üretildi");
+
+	public IRAM CreateRAM()
+		=> new RAM("Toshiba RAM üretildi");
+
+	public IVideoCard CreateVideoCard()
+		=> new VideoCard("Toshiba VideoCard üretildi");
+}
+class MSIFactory : IComputerFactory
+{
+	public ICPU CreateCPU()
+		=> new CPU("MSI CPU üretildi");
+
+	public IRAM CreateRAM()
+		=> new RAM("MSI RAM üretildi");
+
+	public IVideoCard CreateVideoCard()
+		=> new VideoCard("MSI VideoCard üretildi");
+}
+#endregion
+
+#region Creator
+class ComputerCreator
+{
+	ICPU _cpu;
+	IRAM _ram;
+	IVideoCard _videoCard;
+
+	public Computer CreateComputer(IComputerFactory computerFactory)
+	{
+		_cpu = computerFactory.CreateCPU();
+		_ram = computerFactory.CreateRAM();
+		_videoCard = computerFactory.CreateVideoCard();
+		return new(_cpu as CPU, _ram as RAM, _videoCard as VideoCard);
+	}
+}
+#endregion
--- Creational_Patterns/Factory_Method_Design_Pattern_BankExample2/Program.cs
+++ Creational_Patterns/Factory_Method_Design_Pattern_BankExample2/Program.cs
@@ -1,6 +1,4 @@
-﻿//FACTORY METHOD VE SINGLETON BERABER KULLANILDI
-
-
+﻿//SADECE FACTORY METHOD DESIGN PATTERN KULLANILDI
 
 //GarantiBank garantiBank = new("asd", "123");
 //garantiBank.ConnectGaranti();
@@ -18,13 +16,8 @@
 
 BankCreator bankCreator = new();
 GarantiBank? garanti = bankCreator.Create(BankType.Garanti) as GarantiBank;
-VakifBank? vakifBank= bankCreator.Create(BankType.VakifBank) as VakifBank;
-HalkBank? halkBank=bankCreator.Create(BankType.HalkBank) as HalkBank;
-
-//singleton design pattern kullandıktan sonra 2. taleplerde de ilk oluşturulan nesne kullanılmış oldu.
-GarantiBank? garanti2 = bankCreator.Create(BankType.Garanti) as GarantiBank;
-VakifBank? vakifBank2 = bankCreator.Create(BankType.VakifBank) as VakifBank;
-HalkBank? halkBank2 = bankCreator.Create(BankType.HalkBank) as HalkBank;
+VakifBank? vakifBank = bankCreator.Create(BankType.VakifBank) as VakifBank;
+HalkBank? halkBank = bankCreator.Create(BankType.Halkbank) as HalkBank;
 
 
 
@@ -42,17 +35,13 @@ interface IBank
 class GarantiBank : IBank
 {
 	string _userCode, _password;
-	private GarantiBank(string userCode, string password)
+	public GarantiBank(string userCode, string password)
 	{
 		Console.WriteLine($"{nameof(GarantiBank)} nesnesi oluşturuldu.");
 		_userCode = userCode;
 		_password = password;
 	}
-	private static GarantiBank _garantiBank;
 
-	static GarantiBank() => _garantiBank = new("asd","123");
-	public static GarantiBank GetInstance=> _garantiBank;
-	
 	public void ConnectGaranti()
 		=> Console.WriteLine($"{nameof(GarantiBank)} - Connected.");
 	public void SendMoney(int amount)
@@ -62,15 +51,13 @@ class GarantiBank : IBank
 class HalkBank : IBank
 {
 	string _userCode, _password;
-	private HalkBank(string userCode)
+	public HalkBank(string userCode)
 	{
 		Console.WriteLine($"{nameof(HalkBank)} nesnesi oluşturuldu.");
 		_userCode = userCode;
 	}
-	private static HalkBank _halkBank;
-	static HalkBank() => _halkBank = new("xyz");
-	public static HalkBank GetInstance=> _halkBank;
-	public  string Password { set => _password = value; }
+
+	public string Password { set => _password = value; }
 
 	public void Send(int amount, string accountNumber)
 		=> Console.WriteLine($"{amount} money sent.");
@@ -85,17 +72,13 @@ class VakifBank : IBank
 {
 	string _userCode, _email, _password;
 	public bool isAuthentcation { get; set; }
-	private VakifBank(CredentialVakifBank credential, string password)
+	public VakifBank(CredentialVakifBank credential, string password)
 	{
 		Console.WriteLine($"{nameof(VakifBank)} nesnesi oluşturuldu.");
 		_userCode = credential.UserCode;
 		_email = credential.Mail;
 		_password = password;
 	}
-
-	private static VakifBank _vakifBank;
-	static VakifBank() => _vakifBank = new(new() { Mail = "aliogullari@gmail.com", UserCode = "pnr" }, "123");
-	public static VakifBank GetInstance=> _vakifBank;
 	public void ValidateCredential()
 	{
 		if (true) //validating
@@ -117,13 +100,9 @@ interface IBankFactory
 #region Concrete Factories
 class GarantiFactory : IBankFactory
 {
-    private GarantiFactory() { }
-	static GarantiFactory()=>_garantiFactory=new();
-	private static GarantiFactory _garantiFactory;
-	public static GarantiFactory GetInstance => _garantiFactory;
-    public IBank CreateInstance()
+	public IBank CreateInstance()
 	{
-		GarantiBank garanti = GarantiBank.GetInstance;
+		GarantiBank garanti = new("asd", "123");
 		garanti.ConnectGaranti();
 		return garanti;
 	}
@@ -131,13 +110,9 @@ class GarantiFactory : IBankFactory
 
 class HalkBankFactory : IBankFactory
 {
-    public HalkBankFactory() { }
-	static HalkBankFactory() => _halkBankFactory = new();
-	private static HalkBankFactory _halkBankFactory;
-	public static HalkBankFactory GetInstance => _halkBankFactory;
-    public IBank CreateInstance()
+	public IBank CreateInstance()
 	{
-		HalkBank halkBank = HalkBank.GetInstance;
+		HalkBank halkBank = new("xyz");
 		halkBank.Password = "123456";
 		return halkBank;
 	}
@@ -145,14 +120,9 @@ class HalkBankFactory : IBankFactory
 
 class VakifBankFactory : IBankFactory
 {
-	private VakifBankFactory() { }
-	static VakifBankFactory() => _vakifBankFactory = new();
-	private static VakifBankFactory _vakifBankFactory;
-	public static VakifBankFactory GetInstance => _vakifBankFactory;
-	
 	public IBank CreateInstance()
 	{
-		VakifBank vakifBank = VakifBank.GetInstance;
+		VakifBank vakifBank = new(new() { Mail = "aliogullari@gmail.com", UserCode = "pnr" }, "123");
 		vakifBank.ValidateCredential();
 		return vakifBank;
 	}
@@ -162,7 +132,7 @@ class VakifBankFactory : IBankFactory
 #region Creator
 enum BankType
 {
-	Garanti,HalkBank,VakifBank
+	Garanti, Halkbank, VakifBank
 }
 class BankCreator
 {
@@ -170,9 +140,9 @@ class BankCreator
 	{
 		IBankFactory _bankFactory = bankType switch
 		{
-			BankType.VakifBank => VakifBankFactory.GetInstance,
-			BankType.HalkBank => HalkBankFactory.GetInstance,
-			BankType.Garanti=>GarantiFactory.GetInstance
+			BankType.VakifBank => new VakifBankFactory(),
+			BankType.Halkbank => new HalkBankFactory(),
+			BankType.Garanti => new GarantiFactory()
 		};
 		return _bankFactory.CreateInstance();
 	}
