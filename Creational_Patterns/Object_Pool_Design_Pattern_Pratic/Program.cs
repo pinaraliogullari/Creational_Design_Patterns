e7e224fa4931f6e41c2088501e3d29a2e4ae4b2f
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-05-23 16:57:38 +0300

    Object Pool Design Pattern Intro

---
+++ Creational_Patterns/Object_Pool_Design_Pattern_Pratic/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections.Concurrent;
+// Öbcelikle tüm sınıflardan nesneleri havuzuna alabilecek kabiliyete sahip bir generic bir class oluşturmalıyız.
+//Daha sonra bu classın içine istediğimiz nesneyi verebilecek bir metot, verdiğimiz nesneyi havuza ekleyecek bir metot ve bir de bu havuzu temsil eden bir koleksiyon eklemeliyiz.
+
+
+ObjectPool<X> pools= new ObjectPool<X>();
+var x1=pools.Get(() => new X()); //havuzda x1 nesnesi varsa getir. yoksa get fonskiyonunun iindeki expressiona verilip üretilecek.
+//....
+pools.Return(x1);// işimiz bittikten sonra x1 i havuza bırakıyoruz.
+
+var x2= pools.Get(() => new X());
+pools.Return(x2);
+
+Console.WriteLine();
+
+
+//tasarladığımız class
+class ObjectPool<T> where T : class
+{
+    readonly ConcurrentBag<T> _instances; //pool
+    public ObjectPool()
+    {
+		_instances = new();
+    }
+    public T Get(Func<T>? objectGenerator=null)
+    {
+        //havuzdan generic parametrede bildirilen türdeki nesneyi geri döndürmek.
+        return _instances.TryTake(out T instance)? instance:objectGenerator();
+    }
+    public void Return (T instance)
+    {
+        //havuzdan ödünç alınan nesneyi geri iade etmek.
+        _instances.Add(instance);
+
+    }
+}
+class X
+{
+    public int Count { get; set; }
+    public void Write()
+        => Console.WriteLine(Count);
+    public X() 
+        => Console.WriteLine("X üretim maliyeti...");
+
+    ~X()
+        => Console.WriteLine("X imha maliyeti...");
+}
