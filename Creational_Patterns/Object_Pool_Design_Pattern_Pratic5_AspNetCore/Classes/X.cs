c527d3e142a669ba191e63d46b4a00acd5aac5be
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-05-23 18:23:35 +0300

    Object Pool Design Pattern studied on ASP.Net Core Project.

---
+++ Creational_Patterns/Object_Pool_Design_Pattern_Pratic5_AspNetCore/Classes/X.cs
@@ -0,0 +1,16 @@
+﻿namespace Object_Pool_Design_Pattern_Pratic5_AspNetCore.Classes
+{
+	//Microsoft.Extensions.ObjectPool Asp.net core projesine default olarak yüklüdür. sadece program cs te belli bir configurasyon yapmamız gerekir.bkz. Program cs
+	public class X
+	{
+		public int Count { get; set; }
+		public void Write()
+			=>Console.WriteLine(Count);
+		public X()
+		=> Console.WriteLine("X üretim maliyeti...");
+
+		~X()
+		=> Console.WriteLine("X imha maliyeti...");
+		
+    }
+}
---
+++ Creational_Patterns/Object_Pool_Design_Pattern_Pratic5_AspNetCore/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ObjectPool;
+using Object_Pool_Design_Pattern_Pratic5_AspNetCore.Classes;
+
+namespace Object_Pool_Design_Pattern_Pratic5_AspNetCore.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	
+	public class ValuesController : ControllerBase
+	{
+		readonly ObjectPool<X> _pool;
+
+		public ValuesController(ObjectPool<X> pool)
+		{
+			_pool = pool;
+		}
+
+		[HttpGet("[action")]
+		public IActionResult Get1()
+		{
+			var x1= _pool.Get();
+			x1.Count++;
+			_pool.Return(x1);
+			return Ok(x1.Count);
+		}
+
+		[HttpGet("[action]")]
+		public IActionResult Get2()
+		{
+			var x2 = _pool.Get();
+			x2.Count++;
+			_pool.Return(x2);
+			return Ok(x2.Count);
+		}
+	}
+}
